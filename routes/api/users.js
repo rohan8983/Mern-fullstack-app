b7445c56f3d744198940846932733e4fa85ded04
rohan8983 <rohan@peoplelinkvc.com>
2018-08-26 22:36:20 +0530

    done some basics of validation in register

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
+const validateRegisterInput = require("../../validations/register");
 
 //load user model
 const User = require("../../models/User");
@@ -18,9 +19,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 //@desc    Register user
 //@access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
